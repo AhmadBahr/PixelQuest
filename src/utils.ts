33ca1d54591086650c2caefea938cfc818345336
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-08-13 23:46:56 +0300

    adding layer of spawn point

--- src/utils.ts
+++ src/utils.ts
@@ -6,18 +6,41 @@ export async function makeMap(k: KaboomCtx, name: string) {
 
     const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)])
 
-    const spawnPoints: { [key: string]: { x: number, y: number } } = {};
+    const spawnPoints: { [key: string]: { x: number, y: number } [] } = {};
 
     for (const layer of mapData.layers) {
         if (layer.name === "colliders") {
             for (const collider of layer.objects) {
                 map.add([k.area({
                     shape: new k.Rect(k.vec2(0), collider.width, collider.height),
-                    collisionIgnore: ["platform","exit"],
-                }), 
-                collider.name !== "exit" ? k.body({ isStatic: true }) :
-                k.body(), k.pos(collider.x, collider.y), k.rect(collider.width, collider.height)]);
+                    collisionIgnore: ["platform", "exit"],
+                }),
+                collider.name !== "exit" ? k.body({ isStatic: true }) : null,
+                k.pos(collider.x, collider.y),
+                collider.name !== "exit" ? "platoform" : "exit"
+                ]);
             }
+            continue;
+        }
+
+        if (layer.name === "spawnpoints") {
+            for (const spawnPoint of layer.objects) {
+                if(spawnPoint[spawnPoint.name])
+                {
+                    spawnPoints[spawnPoint.name].push ({
+                        x: spawnPoint.x,
+                        y: spawnPoint.y
+                    })
+                    continue
+                }
+                spawnPoints[spawnPoint.name] = [
+                    {
+                        x: spawnPoint.x,
+                        y: spawnPoint.y
+                    }
+                ]
+            }
+            continue;
         }
     }
 }
