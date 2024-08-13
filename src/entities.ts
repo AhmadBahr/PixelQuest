313970180dbc8994d1d85f912ee7f30c349b4108
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-08-14 00:11:30 +0300

    making a player

---
+++ src/entities.ts
@@ -0,0 +1,22 @@
+import { KaboomCtx } from "kaboom";
+import { scale } from "./constants";
+
+export function makePlayer(k: KaboomCtx, posX: number, posy: number) {
+    const player = k.make([
+        k.sprite("assets", { anim: "kirbIdle" }),
+        k.area({ shape: new k.Rect(k.vec2(4, 5.9), 284, 10) }),
+        k.body(),
+        k.pos(posX * scale, posy * scale),
+        k.scale(scale),
+        k.doubleJump(3),
+        k.health(3),
+        k.opacity(1),
+        {
+            speed: 300,
+            direction: "right",
+            isInhaling: false,
+            isFull: false
+        },
+        "player",
+    ]);
+}
